201a036cb48186de047ac9bccdb5cd7dbeb67a08
syuhin <syuhin@163.com>
2020-08-02 22:46:45 +0800

    test syn 3

--- src/com/tutorialspoint/MainApp.java
+++ src/com/tutorialspoint/MainApp.java
@@ -7,8 +7,8 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class MainApp {
 
-    public static void main(String[] args) {
-//        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+    protected static void beanTest(){
+        //        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 //        ApplicationContext context = new FileSystemXmlApplicationContext("D:/ws/code/HelloSpring/src/beans.xml");
         AbstractApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
@@ -23,5 +23,17 @@ public class MainApp {
         HelloWorld objD = (HelloWorld) context.getBean("helloWorldP");
         objD.getMessage();
         context.registerShutdownHook();
+    }
+
+    protected static void aopTest(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Student student = (Student)context.getBean("student");
+        student.getName();
+        student.getAge();
+        student.printThrowException();
+    }
+
+    public static void main(String[] args) {
+        MainApp.aopTest();
     }
 }
