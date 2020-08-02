a350b2aa93ed34214f999232034257ea035e6fdc
syuhin <syuhin@163.com>
2020-08-02 10:01:26 +0800

    test syn 3

--- src/com/tutorialspoint/InitHelloWorld.java
+++ src/com/tutorialspoint/InitHelloWorld.java
@@ -13,7 +13,7 @@ public class InitHelloWorld implements BeanPostProcessor {
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        System.out.println("AfterInitialization222  = " + beanName);
+        System.out.println("AfterInitialization333  = " + beanName);
         return bean;
     }
 }
